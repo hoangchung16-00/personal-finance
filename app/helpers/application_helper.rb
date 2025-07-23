a5f11ad7852bbf1c9715b73f9bd49a7f2d6b1a10
hoangchung16-00 <hoangchung1687@gmail.com>
2025-07-23 15:38:33 +0700

    Update links sign_in/sign_up views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def camelizations_omniauth
+    {
+      google_oauth2: "Google",
+      facebook: "Facebook"
+    }
+  end
+
+  def omniauth_action(controller_name)
+    controller_name == "registrations" ? "Register" : "Sign in"
+  end
 end
